fe8478679040c08ad05fa2791b96a4b3d68933aa
karinacristo <karinacristonegocios@gmail.com>
2021-11-24 17:44:04 -0300

    Organizando arquivos em pastas.

---
+++ Arquivos js/Personagem 2/scriptFaseUm.js
@@ -0,0 +1,23 @@
+function decidirOpcao(){
+    var escolha
+    var opcaoUm = ('1')
+    var opcaoDois = ('2')
+
+escolha = prompt("Decida o que fazer! Digite o número correspondente:\n1\n2")
+while(true){
+    if(escolha == '1'){
+        location= "http://127.0.0.1:5500/Arquivos%20html/Personagem%202%20html/Fase%202/faseDois.html"
+        break
+
+    }else if(escolha == '2'){
+        location = "http://127.0.0.1:5500/Arquivos%20html/Personagem%202%20html/Fase%201/gameoverFaseUm.html"
+        break
+        
+    }else{
+        alert("Número inválido")
+        escolha = prompt("Decida o que fazer! Digite o número correspondente:\n1\n2")
+    }
+}
+}
+
+setTimeout(decidirOpcao, 3000)
---
+++ Arquivos js/script.js
@@ -0,0 +1,29 @@
+function decidirPersonagens(){
+   var personagemEscolhido
+    var personagemUm = ('Caitlyn')
+    var personagemDois = ('Mel')
+    var personagemTres = ('Vander')
+
+personagemEscolhido = prompt("Escolha seu personagem! Digite o nome correspondente:\n- Caitlyn\n- Mel\n- Vander")
+while(true){
+    if(personagemEscolhido == 'Caitlyn'){
+        location= "http://127.0.0.1:5500/Arquivos%20html/Personagem%201%20html/caitlynIntroducao.html"
+        break;
+
+    }else if(personagemEscolhido == 'Mel'){
+        location = "http://127.0.0.1:5500/Arquivos%20html/Personagem%202%20html/melIntroducao.html"
+        break;
+
+    }else if(personagemEscolhido == 'Vander'){
+        location = "http://127.0.0.1:5500/Arquivos%20html/Personagem%203%20html/vanderIntroducao.html"
+        break;
+        
+    }else{
+        alert("Personagem inválido")
+        personagemEscolhido = prompt("Escolha seu personagem! Digite o nome correspondente:\n- Caitlyn\n- Mel\n- Vander")
+    }
+    
+}
+}
+
+setTimeout(decidirPersonagens, 2000);
